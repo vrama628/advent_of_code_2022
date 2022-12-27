c66f1aa62747d99f28d33e581f6ccb7bec22a16f
Vijay Ramamurthy <vrama628@gmail.com>
2022-12-26 19:20:04 -0600

    more day 16

--- src/day16pt2.ml
+++ src/day16pt2.ml
@@ -5,6 +5,16 @@ type valve = {
   rate : int;
 }
 
+module MemoKey = struct
+  type t = {
+    human_valve : string;
+    phant_valve : string;
+    time : int;
+    opened : string list;
+  }
+  [@@deriving ord, sexp_of, hash]
+end
+
 let () =
   let valves =
     In_channel.input_lines In_channel.stdin
@@ -28,122 +38,101 @@ let () =
            )
          )
   in
-  let rec travel
-      elven_valve
-      phant_valve
-      time
-      opened_at
-      elven_last_seen
-      phant_last_seen
-      alpha =
-    let num_opened = Map.length opened_at in
-    if
-      time >= 26
-      || List.equal String.equal (Map.keys opened_at) (Map.keys valves)
-      || Option.equal
-           ( = )
-           (Map.find elven_last_seen elven_valve)
-           (Some num_opened)
-      || Option.equal
-           ( = )
-           (Map.find phant_last_seen phant_valve)
-           (Some num_opened)
-      || Option.equal
-           ( = )
-           (Map.find phant_last_seen elven_valve)
-           (Some num_opened)
-         && Option.equal
-              ( = )
-              (Map.find elven_last_seen phant_valve)
-              (Some num_opened)
-      || alpha
-         >= Map.fold valves ~init:0 ~f:(fun ~key ~data acc ->
+  let num_valves = Map.length valves in
+  let memo_table = Hashtbl.create (module MemoKey) in
+  let rec travel human_valve phant_valve time opened report =
+    Hashtbl.find_or_add
+      memo_table
+      MemoKey.{ human_valve; phant_valve; time; opened = Set.to_list opened }
+      ~default:(fun () ->
+        if time >= 26 || Set.length opened = num_valves then
+          0
+        else
+          let human_moves =
+            (Map.find_exn valves human_valve).tunnels
+            |> List.map ~f:(fun tunnel -> tunnel, [])
+          in
+          let phant_moves =
+            (Map.find_exn valves phant_valve).tunnels
+            |> List.map ~f:(fun tunnel -> tunnel, [])
+          in
+          let human_choices =
+            if
+              Set.mem opened human_valve
+              || (Map.find_exn valves human_valve).rate = 0
+            then
+              human_moves
+            else
+              (human_valve, [human_valve]) :: human_moves
+          in
+          let phant_choices =
+            if
+              Set.mem opened phant_valve
+              || (Map.find_exn valves phant_valve).rate = 0
+            then
+              phant_moves
+            else
+              (phant_valve, [phant_valve]) :: phant_moves
+          in
+          let all_choices =
+            List.cartesian_product human_choices phant_choices
+            |> List.permute
+            |>
+            if String.(human_valve = phant_valve) then
+              List.filter
+                ~f:(fun ((human_valve, human_opens), (phant_valve, phant_opens))
+                   ->
+                  String.(human_valve <= phant_valve)
+                  && (List.is_empty human_opens || List.is_empty phant_opens)
+              )
+            else
+              Fn.id
+          in
+          let num_choices = List.length all_choices in
+          List.foldi
+            all_choices
+            ~init:0
+            ~f:(fun
+                 i
+                 acc
+                 ((human_valve, human_opens), (phant_valve, phant_opens))
+               ->
+              let opened, additional_flow =
+                List.fold
+                  (human_opens @ phant_opens)
+                  ~init:(opened, 0)
+                  ~f:(fun (opened, flow) valve ->
+                    ( Set.add opened valve,
+                      flow
+                      + ((Map.find_exn valves valve).rate * (26 - (time + 1))) )
+                )
+              in
+              let report f = report ((float i +. f) /. float num_choices) in
+              report 0.;
+              max
                 acc
-                + (26
-                  - (Map.find opened_at key |> Option.value ~default:(time + 1))
-                  )
-                  * data.rate
-            )
-    then
-      Map.fold opened_at ~init:0 ~f:(fun ~key ~data acc ->
-          acc + ((Map.find_exn valves key).rate * (26 - data))
+                (additional_flow
+                + travel human_valve phant_valve (time + 1) opened report
+                )
+          )
       )
-    else
-      let () =
-        if time < 10 then
-          printf
-            "%selven=%s, phant=%s\n%!"
-            (String.init time ~f:(Fn.const ' '))
-            elven_valve
-            phant_valve
-      in
-      let phant_last_seen =
-        Map.set phant_last_seen ~key:phant_valve ~data:(Map.length opened_at)
-      in
-      let elven_last_seen =
-        Map.set elven_last_seen ~key:elven_valve ~data:(Map.length opened_at)
-      in
-      let elven_moves =
-        (Map.find_exn valves elven_valve).tunnels
-        |> List.map ~f:(fun tunnel -> tunnel, [])
-      in
-      let phant_moves =
-        (Map.find_exn valves phant_valve).tunnels
-        |> List.map ~f:(fun tunnel -> tunnel, [])
-      in
-      let elven_choices =
-        if
-          Map.mem opened_at elven_valve
-          || (Map.find_exn valves elven_valve).rate = 0
-        then
-          elven_moves
-        else
-          (elven_valve, [elven_valve]) :: elven_moves
-      in
-      let phant_choices =
-        if
-          Map.mem opened_at phant_valve
-          || (Map.find_exn valves phant_valve).rate = 0
-        then
-          phant_moves
-        else
-          (phant_valve, [phant_valve]) :: phant_moves
-      in
-      List.cartesian_product elven_choices phant_choices
-      |> List.permute
-      |> ( if String.(elven_valve = phant_valve) then
-           List.filter
-             ~f:(fun ((elven_valve, elven_opens), (phant_valve, phant_opens)) ->
-               String.(elven_valve <= phant_valve)
-               && (List.is_empty elven_opens || List.is_empty phant_opens)
-           )
-         else
-           Fn.id
-         )
-      |> List.fold
-           ~init:alpha
-           ~f:(fun alpha ((elven_valve, elven_opens), (phant_valve, phant_opens))
-              ->
-             let opened_at =
-               List.fold
-                 (elven_opens @ phant_opens)
-                 ~init:opened_at
-                 ~f:(fun opened_at key ->
-                   Map.set opened_at ~key ~data:(time + 1)
-               )
-             in
-             max
-               alpha
-               (travel
-                  elven_valve
-                  phant_valve
-                  (time + 1)
-                  opened_at
-                  elven_last_seen
-                  phant_last_seen
-                  alpha
-               )
-         )
   in
-  let empty = Map.empty (module String) in
-  printf "%d\n" (travel "AA" "AA" 0 empty empty empty 2265)
+  let start_time = Time.now () in
+  let zone = Time.Zone.of_utc_offset ~hours:(-6) in
+  let report f =
+    if Float.(Random.float 1. < 0.00001) then
+      let finish_time =
+        try
+          Time.add
+            start_time
+            Core_private.Span_float.(Time.diff (Time.now ()) start_time / f)
+          |> Time.to_sec_string ~zone
+        with Failure s -> s
+      in
+      printf
+        "\r%.16f\t%20d\tExpected to finish at %s%!"
+        f
+        (Hashtbl.length memo_table)
+        finish_time
+  in
+  printf "\n%d\n" (travel "AA" "AA" 0 (Set.empty (module String)) report)
